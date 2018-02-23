7bd8dd65a8f2ac9552e70c747bc6eb4ba3826cfb
leonardoADS123 <36766846+leonardoADS123@users.noreply.github.com>
2018-02-23 11:19:08 -0300

    primeira aula
    primeira aula

---
+++ PrimeiraAula/PrimeiraAula/Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace PrimeiraAula
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblPrimeiroNumero = new System.Windows.Forms.Label();
+            this.txbPrimeiroNumero = new System.Windows.Forms.TextBox();
+            this.txbSegundoNumero = new System.Windows.Forms.TextBox();
+            this.lblSegundoNumero = new System.Windows.Forms.Label();
+            this.btnSomar = new System.Windows.Forms.Button();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.btnSubtrair = new System.Windows.Forms.Button();
+            this.btnDividir = new System.Windows.Forms.Button();
+            this.btnMultiplicar = new System.Windows.Forms.Button();
+            this.txbResultado = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblPrimeiroNumero
+            // 
+            this.lblPrimeiroNumero.AutoSize = true;
+            this.lblPrimeiroNumero.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblPrimeiroNumero.Location = new System.Drawing.Point(76, 9);
+            this.lblPrimeiroNumero.Name = "lblPrimeiroNumero";
+            this.lblPrimeiroNumero.Size = new System.Drawing.Size(53, 13);
+            this.lblPrimeiroNumero.TabIndex = 0;
+            this.lblPrimeiroNumero.Text = "Numero 1";
+            // 
+            // txbPrimeiroNumero
+            // 
+            this.txbPrimeiroNumero.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.txbPrimeiroNumero.Location = new System.Drawing.Point(79, 25);
+            this.txbPrimeiroNumero.Name = "txbPrimeiroNumero";
+            this.txbPrimeiroNumero.Size = new System.Drawing.Size(117, 20);
+            this.txbPrimeiroNumero.TabIndex = 1;
+            this.toolTip1.SetToolTip(this.txbPrimeiroNumero, "Digite um numero");
+            // 
+            // txbSegundoNumero
+            // 
+            this.txbSegundoNumero.BackColor = System.Drawing.Color.White;
+            this.txbSegundoNumero.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txbSegundoNumero.Location = new System.Drawing.Point(79, 86);
+            this.txbSegundoNumero.Name = "txbSegundoNumero";
+            this.txbSegundoNumero.Size = new System.Drawing.Size(117, 20);
+            this.txbSegundoNumero.TabIndex = 3;
+            this.toolTip1.SetToolTip(this.txbSegundoNumero, "Digite outro numero");
+            // 
+            // lblSegundoNumero
+            // 
+            this.lblSegundoNumero.AutoSize = true;
+            this.lblSegundoNumero.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblSegundoNumero.Location = new System.Drawing.Point(76, 70);
+            this.lblSegundoNumero.Name = "lblSegundoNumero";
+            this.lblSegundoNumero.Size = new System.Drawing.Size(53, 13);
+            this.lblSegundoNumero.TabIndex = 2;
+            this.lblSegundoNumero.Text = "Numero 2";
+            // 
+            // btnSomar
+            // 
+            this.btnSomar.BackColor = System.Drawing.Color.MidnightBlue;
+            this.btnSomar.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnSomar.Location = new System.Drawing.Point(36, 162);
+            this.btnSomar.Name = "btnSomar";
+            this.btnSomar.Size = new System.Drawing.Size(75, 23);
+            this.btnSomar.TabIndex = 4;
+            this.btnSomar.Text = "+";
+            this.toolTip1.SetToolTip(this.btnSomar, "Aperte o botão para somar");
+            this.btnSomar.UseVisualStyleBackColor = false;
+            this.btnSomar.Click += new System.EventHandler(this.btnSomar_Click);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblResultado.Location = new System.Drawing.Point(12, 272);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(55, 13);
+            this.lblResultado.TabIndex = 5;
+            this.lblResultado.Text = "Resultado";
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.BackColor = System.Drawing.Color.Blue;
+            this.btnLimpar.Location = new System.Drawing.Point(106, 206);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpar.TabIndex = 6;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = false;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // btnSubtrair
+            // 
+            this.btnSubtrair.BackColor = System.Drawing.Color.MidnightBlue;
+            this.btnSubtrair.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnSubtrair.Location = new System.Drawing.Point(36, 125);
+            this.btnSubtrair.Name = "btnSubtrair";
+            this.btnSubtrair.Size = new System.Drawing.Size(75, 23);
+            this.btnSubtrair.TabIndex = 7;
+            this.btnSubtrair.Text = "-";
+            this.toolTip1.SetToolTip(this.btnSubtrair, "Aperte o botão para somar");
+            this.btnSubtrair.UseVisualStyleBackColor = false;
+            this.btnSubtrair.Click += new System.EventHandler(this.btnSubtrair_Click);
+            // 
+            // btnDividir
+            // 
+            this.btnDividir.BackColor = System.Drawing.Color.MidnightBlue;
+            this.btnDividir.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnDividir.Location = new System.Drawing.Point(186, 125);
+            this.btnDividir.Name = "btnDividir";
+            this.btnDividir.Size = new System.Drawing.Size(75, 23);
+            this.btnDividir.TabIndex = 8;
+            this.btnDividir.Text = "/";
+            this.toolTip1.SetToolTip(this.btnDividir, "Aperte o botão para somar");
+            this.btnDividir.UseVisualStyleBackColor = false;
+            this.btnDividir.Click += new System.EventHandler(this.btnDividir_Click);
+            // 
+            // btnMultiplicar
+            // 
+            this.btnMultiplicar.BackColor = System.Drawing.Color.MidnightBlue;
+            this.btnMultiplicar.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnMultiplicar.Location = new System.Drawing.Point(186, 162);
+            this.btnMultiplicar.Name = "btnMultiplicar";
+            this.btnMultiplicar.Size = new System.Drawing.Size(75, 23);
+            this.btnMultiplicar.TabIndex = 9;
+            this.btnMultiplicar.Text = "*";
+            this.toolTip1.SetToolTip(this.btnMultiplicar, "Aperte o botão para somar");
+            this.btnMultiplicar.UseVisualStyleBackColor = false;
+            this.btnMultiplicar.Click += new System.EventHandler(this.btnMultiplicar_Click);
+            // 
+            // txbResultado
+            // 
+            this.txbResultado.BackColor = System.Drawing.Color.White;
+            this.txbResultado.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txbResultado.Location = new System.Drawing.Point(79, 265);
+            this.txbResultado.Name = "txbResultado";
+            this.txbResultado.ReadOnly = true;
+            this.txbResultado.Size = new System.Drawing.Size(117, 20);
+            this.txbResultado.TabIndex = 10;
+            this.toolTip1.SetToolTip(this.txbResultado, "resultado");
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(290, 313);
+            this.Controls.Add(this.txbResultado);
+            this.Controls.Add(this.btnMultiplicar);
+            this.Controls.Add(this.btnDividir);
+            this.Controls.Add(this.btnSubtrair);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.btnSomar);
+            this.Controls.Add(this.txbSegundoNumero);
+            this.Controls.Add(this.lblSegundoNumero);
+            this.Controls.Add(this.txbPrimeiroNumero);
+            this.Controls.Add(this.lblPrimeiroNumero);
+            this.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Name = "frmPrincipal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Somar Dois Números";
+            this.Load += new System.EventHandler(this.frmPrincipal_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblPrimeiroNumero;
+        private System.Windows.Forms.TextBox txbPrimeiroNumero;
+        private System.Windows.Forms.TextBox txbSegundoNumero;
+        private System.Windows.Forms.Label lblSegundoNumero;
+        private System.Windows.Forms.Button btnSomar;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Button btnSubtrair;
+        private System.Windows.Forms.Button btnDividir;
+        private System.Windows.Forms.Button btnMultiplicar;
+        private System.Windows.Forms.TextBox txbResultado;
+
+    }
+}
+
---
+++ PrimeiraAula/PrimeiraAula/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PrimeiraAula
+{
+    public partial class frmPrincipal : Form
+    {
+        public frmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void frmPrincipal_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSomar_Click(object sender, EventArgs e)
+        {
+
+
+            Double n1 = 0.0, n2 = 0.0, resultado = 0.0;
+            n1 = Convert.ToDouble(txbPrimeiroNumero.Text);
+            n2 = Convert.ToDouble(txbSegundoNumero.Text);
+            resultado = n1 + n2;
+            txbResultado.Text = resultado.ToString();
+
+
+
+
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            txbPrimeiroNumero.Text = "";
+            txbSegundoNumero.Text = "";
+            txbResultado.Text = "";
+        }
+
+        private void btnSubtrair_Click(object sender, EventArgs e)
+        {
+            Double n1 = 0.0, n2 = 0.0, resultado = 0.0;
+            n1 = Convert.ToDouble(txbPrimeiroNumero.Text);
+            n2 = Convert.ToDouble(txbSegundoNumero.Text);
+            resultado = n1 - n2;
+            txbResultado.Text = resultado.ToString();
+        }
+
+        private void btnDividir_Click(object sender, EventArgs e)
+        {
+            Double n1 = 0.0, n2 = 0.0, resultado = 0.0;
+            n1 = Convert.ToDouble(txbPrimeiroNumero.Text);
+            n2 = Convert.ToDouble(txbSegundoNumero.Text);
+            resultado = n1 / n2;
+            txbResultado.Text = resultado.ToString();
+
+            if (n2 == 0) 
+            {
+                MessageBox.Show("não é possivel dividir por zero");
+
+
+            }
+        }
+
+        private void btnMultiplicar_Click(object sender, EventArgs e)
+        {
+            Double n1 = 0.0, n2 = 0.0, resultado = 0.0;
+            n1 = Convert.ToDouble(txbPrimeiroNumero.Text);
+            n2 = Convert.ToDouble(txbSegundoNumero.Text);
+            resultado = n1 * n2;
+            txbResultado.Text = resultado.ToString();
+        }
+    }
+}
